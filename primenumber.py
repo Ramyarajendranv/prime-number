a33b6cea4fde4b4e589a0cce4e74e57cf9ee6e1e
Ramya Rajendran <128841339+Ramyarajendranv@users.noreply.github.com>
2023-03-25 07:49:23 +0530

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,3 +1,13 @@
+#prime number
+'''n=int(input("Enter the number:"))
+count=0
+for i in range(1,n+1):
+    if(n%i==0):
+        count=count+1
+if(count==2):
+    print("prime number")
+else:
+    print("not prime number")'''
 
 #prime number list
 n=int(input("Enter the number:"))
