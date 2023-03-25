476f542066ef646ff1aa5e7ce36856c0f00100cf
Ramya Rajendran <128841339+Ramyarajendranv@users.noreply.github.com>
2023-03-25 07:43:29 +0530

    finding the list of prime number

--- primenumber.py
+++ primenumber.py
@@ -1,10 +1,10 @@
-#prime number
+
+#prime number list
 n=int(input("Enter the number:"))
-count=0
-for i in range(1,n+1):
-    if(n%i==0):
-        count=count+1
-if(count==2):
-    print("prime number")
-else:
-    print("not prime number")
+for i in range(2,n+1):
+    for j in range(2,i):
+        if(i%j==0):
+           break
+    else:
+        print(i)
+
